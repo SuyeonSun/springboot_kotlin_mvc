e25a4ca28ead99f9c27a38dd74c80b01188de37f
SuyeonSun <suninisy@gmail.com>
2023-02-18 09:29:07 +0900

    GetMapping 테스트 함수 작성

---
+++ src/main/kotlin/com/example/spring_boot_crud/SpringBootCrudApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_boot_crud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootCrudApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootCrudApplication>(*args)
+}
